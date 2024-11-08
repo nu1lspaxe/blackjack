d0761f49b81d0287958fdc25a7a173ed0bc5d2f5
junxiangzhan <96927456+junxiangzhan@users.noreply.github.com>
2024-11-08 22:54:22 +0800

    Code Tidy Up

--- client/src/screens/main-menu/index.tsx
+++ client/src/screens/main-menu/index.tsx
@@ -2,7 +2,6 @@ import { FunctionComponent } from "react";
 
 import Brand from "@screens/brand.svg?react";
 
-// import Style from "./index.module.css";
 import ScreensStyle from "@screens/screens.module.css";
 
 export const MainMenu: FunctionComponent = function () {
--- client/src/screens/main-screen/index.tsx
+++ client/src/screens/main-screen/index.tsx
@@ -1,11 +1,11 @@
 import { FunctionComponent } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { classname } from "@/utils";
 import Brand from "@screens/brand.svg?react";
 
 import Style from "./index.module.css";
 import ScreensStyle from "@screens/screens.module.css";
-import { classname } from "@/utils";
 
 export const MainMenu: FunctionComponent = function () {
     const navigate = useNavigate();
