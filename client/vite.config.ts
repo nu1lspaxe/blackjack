bb9962b601181f34fb0dc814bd033d6f805886d6
junxiangzhan <96927456+junxiangzhan@users.noreply.github.com>
2024-11-08 22:48:25 +0800

    Project Setting

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            "@": resolve("./src"),
+            "@components": resolve("./src/components"),
+            "@screens": resolve("./src/screens"),
+            "@utils": resolve("./src/utils")
+        }
+    },
+
+    plugins: [react(), svgr()],
+});
