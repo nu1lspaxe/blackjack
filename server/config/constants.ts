1d1a07bc238a12eabca65b48e2dc3e60527abfd9
Nu1LSpaxe <82641653+nu1lspaxe@users.noreply.github.com>
2024-11-12 16:45:58 +0800

    feat: append utils

--- server/config/constants.ts
+++ server/config/constants.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 export const constants = {
     PORT: process.env.PORT || 5555,
+    WS_PORT: parseInt(process.env.WS_PORT || "8888", 10),
     MONGO_URI: process.env.MONGO_URI || '',
     MONGO_X509: path.resolve(__dirname, process.env.MONGO_X509 || ''),
     MONGO_DB: process.env.MONGO_DB || 'test',
--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -3,3 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 export const generateUUID = () => {
   return uuidv4();
 };
+
+export const generateTableCode = (): string => {
+  const tableCode = Math.floor(Math.random() * 900000) + 100000; 
+  return tableCode.toString();
+}
+
+export const ERROR = {
+  INVALID_VALUE: 'Invalid value',
+  UNDEFINED_CARD: 'Undefined card',
+  INVALID_ROOM: 'Room not found',
+  ROOM_FULL: 'Room is full',
+}
